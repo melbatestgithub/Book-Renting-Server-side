4e6ecf6d6cf1eabe8fe57841ddb00990c75bdb52
melakuzeleke <melakuzeleke443@gmail.com>
2024-08-13 13:39:51 +0300

    change

--- controller/UploadedBook.js
+++ controller/UploadedBook.js
@@ -18,28 +18,35 @@ exports.getOwnerUploadedBook = async (req, res) => {
 
 // Update an uploaded book
 exports.UpdateUploadedBook = async (req, res) => {
-    const { id } = req.params;
-    const { book_name, status, book_price, book_number } = req.body;
-  
-    try {
-      const updatedBook = await prisma.uploadedBook.update({
-        where: { id: parseInt(id) },
-        data: {
-          book_name,
-          status,
-          book_price,
-          book_number,
-        },
-      });
-  
-      res.status(200).json(updatedBook);
-    } catch (error) {
-      if (error.code === 'P2025') { // Prisma specific error code for record not found
-        return res.status(404).json({ message: 'Book not found' });
-      }
-      console.error("Update error:", error);
-      res.status(500).json({ message: 'An error occurred while updating the book' });
-    }
+    const user = req.user; // Retrieve user from localStorage
+    const bookId = parseInt(req.params.bookId);
+
+    // const ability = defineAbilitiesFor(user);
+
+ 
+        const updatedBookData = {
+            book_name: req.body.book_name,
+            author: req.body.author,    
+            book_price: req.body.book_price,
+            book_number: req.body.book_number
+        };
+
+        try {
+            const updatedBook = await prisma.uploadedBook.update({
+                where: { id: bookId },
+                data: updatedBookData
+            });
+
+            if (updatedBook) {
+                res.status(200).send('Book updated successfully');
+            } else {
+                res.status(404).send('Book not found');
+            }
+        }
+        
+        catch (error) {
+            res.status(500).send('Error updating the book');
+        }
 
     
 };
